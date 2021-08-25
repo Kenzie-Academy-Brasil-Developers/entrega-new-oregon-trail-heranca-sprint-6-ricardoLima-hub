98e3c0eec6055fd052fef6977e28bbb46a434691
Ricardo Lima <ricardo.engp@gmail.com>
2021-08-25 19:12:31 -0300

    initial commit

--- models/Traveler.js
+++ models/Traveler.js
@@ -1,5 +1,45 @@
 class Traveler {
+    constructor (name, isHealthy = "true", food = 1) {
+        this._name = name;
+        this._amountFood = Number(food);
+        this._isHealthy = isHealthy;
+    }
 
+    get amountFood () {
+        return this._amountFood
+    }
+
+    get name () {
+        return this._name
+    }
+
+    get health () {
+        return this._isHealthy
+    }
+
+    set amountFood (value) {
+        this._amountFood = value
+    }
+
+    set name(value){
+        this._name = value
+    }
+
+    set health (value) {
+        this._isHealthy = value
+    }
+
+    hunt() {
+        this.amountFood += 2;
+    }
+
+    eat() {
+        if (this.amountFood > 0) {
+            return this.amountFood--;
+        } else {
+            return this.isHealthy = false;
+        }
+    }
 }
 
 module.exports = Traveler;
--- models/Wagon.js
+++ models/Wagon.js
@@ -1,5 +1,56 @@
 class Wagon {
+    constructor (capacity) {
+        this._capacity = capacity;
+        this._passengers = [];
+    }
 
+    get capacity () {
+        return this._capacity
+    }
+
+    get passengers () {
+        return this._passengers
+    }
+
+    set capacity (value) {
+        this._capacity = value
+    }
+
+    set passengers (value) {
+        this._passengers = value
+    }
+    getAvailableSeatCount() {
+       if (this.passengers.length === 0) {
+           return this.capacity
+       } 
+       if (this.passengers.length !== 0) {
+           return this.capacity - this.passengers.length
+       }
+    }
+
+    join (traveler) {
+        let seats = this.getAvailableSeatCount()
+        if (seats >= 1) {
+            this.passengers.push(traveler)
+        }
+    }
+
+    shouldQuarantine () {
+        let personSick = false;
+        this.passengers.forEach((element) => {
+          if (element.isHealthy !== true) {
+            personSick = true;
+          }
+        });
+        return personSick;
+      };
+
+    totalFood () {
+        let output = this.passengers.reduce((totalFood, {amountFood}) => {
+            return totalFood + amountFood
+        },0)
+        return output
+    }
 }
 
 module.exports = Wagon;
